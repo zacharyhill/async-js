a85418b42bb7a2a25a00608c9bbd9e33fc506324
Shi-Hao Hong <shihaohong@Shi-Haos-MacBook-Air.local>
2018-04-02 22:13:26 -0700

    added more comments and helper fn

--- async-all-word-count.js
+++ async-all-word-count.js
@@ -11,12 +11,50 @@ However, you now have the power of code to do it within an instant and
 can do it in a fraction of a second without her knowing :) 
 
 TLDR; Write a function that reads an input file and outputs a .txt file 
-containing the word count of every word in your input.txt
+containing the word count of every word in your input.txt. The ordering
+of the words in the file does not matter. This is purely to practice 
+use of asynchronous functions! 
+
+Contraints: 
+1) No punctuation will be in your paragraph
+2) This is case-insensitive, so apple and Apple both count as the same word
 
 /*                                                  */
 
 const fs = require('fs');
 
-var countAllWords = function(filePath) {
+// countWords is a helper function... it helps you convert a paragraph into 
+// the string you need
+
+// EXAMPLE USAGE 
+// countWords('hello world hello me') --> 'hello: 2\nme:1\nworld: 1\n'
+const countWords = function(paragraph) {
+  let words = paragraph.trim('.').split(' ');
+
+  let wordBank = {};
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i].toLowerCase();
+    if (wordBank.hasOwnProperty(word)) {
+      wordBank[word] += 1;
+    } else {
+      wordBank[word] = 1;
+    }
+  }
+
+  let outputString = '';
+
+  let wordsFound = Object.keys(wordBank).sort();
+
+  wordsFound.forEach(word => {
+    outputString += `${word}: ${wordBank[word]}\n`;
+  })
+  
+  return outputString;
+}
+
+// inputFile: paragraph to read
+// outputFile: path to write resulting .txt to
+var countAllWords = function(inputFile, outputFile) {
   /* WRITE CODE HERE */ 
 }
