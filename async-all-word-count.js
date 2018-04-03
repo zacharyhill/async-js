fca3de4247bf6e73c8f7264fe87d6ab4e1a98401
Shi-Hao Hong <shihaohong@Shi-Haos-MacBook-Air.local>
2018-04-02 21:52:00 -0700

    init

---
+++ async-all-word-count.js
@@ -0,0 +1,22 @@
+/*                                                  
+Your grade school Math teacher visited you at Hack Reactor, expecting
+you to do an old assignment to see if you're still a smart cookie. 
+In fact, your teacher expects you to do this every week to make sure 
+you keep practiced! How frustrating! 
+
+The assignment: Count the number of distinct words in a paragraph and 
+submit it to her. 
+
+However, you now have the power of code to do it within an instant and
+can do it in a fraction of a second without her knowing :) 
+
+TLDR; Write a function that reads an input file and outputs a .txt file 
+containing the word count of every word in your input.txt
+
+/*                                                  */
+
+const fs = require('fs');
+
+var countAllWords = function(filePath) {
+  /* WRITE CODE HERE */ 
+}
