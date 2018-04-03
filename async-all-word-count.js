97d0ae12fed98ba42a4301908077bb43628cd286
Shi-Hao Hong <shihaohong@Shi-Haos-MacBook-Air.local>
2018-04-02 22:21:27 -0700

    added more comments and an example output txt file

--- async-all-word-count.js
+++ async-all-word-count.js
@@ -1,4 +1,6 @@
-/*                                                  
+/*        
+   ----- unnecessary background story -----    
+
 Your grade school Math teacher visited you at Hack Reactor, expecting
 you to do an old assignment to see if you're still a smart cookie. 
 In fact, your teacher expects you to do this every week to make sure 
@@ -10,14 +12,20 @@ submit it to her.
 However, you now have the power of code to do it within an instant and
 can do it in a fraction of a second without her knowing :) 
 
-TLDR; Write a function that reads an input file and outputs a .txt file 
-containing the word count of every word in your input.txt. The ordering
-of the words in the file does not matter. This is purely to practice 
-use of asynchronous functions! 
+----- end of unnecessary background story ----- 
 
-Contraints: 
+Write a function that reads an input file and outputs a .txt file 
+containing the word count of every word in your input.txt. 
+This is purely to practice use of asynchronous functions! 
+See exampleOutput folder's sampleOutput.txt for what your output should look like
+with the given input.txt
+
+Constraints: 
 1) No punctuation will be in your paragraph
 2) This is case-insensitive, so apple and Apple both count as the same word
+
+Allowed documentation: 
+- Node.js documentation for FS library (https://nodejs.org/api/fs.html)
 
 /*                                                  */
 
@@ -27,7 +35,7 @@ const fs = require('fs');
 // the string you need
 
 // EXAMPLE USAGE 
-// countWords('hello world hello me') --> 'hello: 2\nme:1\nworld: 1\n'
+// countWords('hello world Hello me') --> 'hello: 2\nme:1\nworld: 1\n'
 const countWords = function(paragraph) {
   let words = paragraph.trim('.').split(' ');
 
@@ -54,7 +62,9 @@ const countWords = function(paragraph) {
 }
 
 // inputFile: paragraph to read
-// outputFile: path to write resulting .txt to
+// outputFile: path to write resulting txt file to
+// EXAMPLE USAGE
+// countAllWords('./input.txt', './output.txt') --> should output a .txt file in same directory
 var countAllWords = function(inputFile, outputFile) {
   /* WRITE CODE HERE */ 
 }
